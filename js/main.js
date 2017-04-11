71fc944c93f9e28c8c34008bcd2b9bbb7be613c7
Matthew McCord <mattm.eternal@gmail.com>
2017-04-11 14:01:17 -0500

    Vue Basic directives

--- js/main.js
+++ js/main.js
@@ -4,3 +4,47 @@ let app = new Vue({
 		message: 'Hello World'
 	}
 });
+
+let app2 = new Vue({
+	el: "#app2",
+	data: {
+		message: `You loaded this page on ${new Date()}`
+	}
+});
+
+let app3 = new Vue({
+	el: "#app3",
+	data: {
+		seen: true
+	}
+});
+
+let app4 = new Vue({
+	el: "#app4",
+	data: {
+		todos: [
+			{ text: 'Learn JS' },
+			{ text: 'Learn Vue' },
+			{ text: 'Build some stuff' }
+		]
+	}
+});
+
+let app5 = new Vue({
+	el: "#app5",
+	data: {
+		message: 'Hello World!'
+	},
+	methods: {
+		reverseMessage: function() {
+			this.message = this.message.split('').reverse().join('');
+		}
+	}
+});
+
+let app6 = new Vue({
+	el: "#app6",
+	data: {
+		message: "Hello Worldo!"
+	}
+});
